a51a4638b38efaf661163a4d11c6881171b299d4
寿振仁 <1146660681@qq.com>
2022-03-16 21:42:36 +0800

    2022.03.16

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="height: 50px;line-height: 50px;border-bottom: 1px solid #cccccc;display: flex;position: relative">
+    <div style="width: 200px;padding-left: 10px;font-weight: bolder;font-size:20px;font-family: 黑体;color: #409eff">后台管理系统</div>
+    <div style="flex: 1"></div>
+    <div style="width:100px">
+      <el-dropdown style="position: absolute;top:10px;right: 20px">
+        <el-button type="primary" >
+          寿振仁<el-icon class="el-icon--right"><arrow-down /></el-icon>
+        </el-button>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item>退出系统</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ArrowDown} from '@element-plus/icons-vue'
+export default {
+  name: "Header",
+  components:{
+    ArrowDown
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="date" label="Date"  />
+      <el-table-column prop="name" label="Name"  />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+  components:{
+
+  },
+  data(){
+    return{
+      tabledata:[]
+    }
+  }
+}
+
+
+</script>
